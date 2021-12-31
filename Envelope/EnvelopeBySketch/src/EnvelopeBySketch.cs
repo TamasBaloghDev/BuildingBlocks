1a8fbfd2afcbf1413fee2223f8b5d64bfb67dd9b
Tamas Balogh <tbalogh@pelogic.io>
2021-12-31 14:45:01 +0100

    Csproj file updates.

--- Envelope/EnvelopeBySketch/src/EnvelopeBySketch.cs
+++ Envelope/EnvelopeBySketch/src/EnvelopeBySketch.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System;
-using Elements;
 using Elements.Geometry;
+using Elements.Geometry.Solids;
+using Elements;
+
 
 namespace EnvelopeBySketch
 {
